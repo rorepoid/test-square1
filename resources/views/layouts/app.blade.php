2c215182190f31e01b0e0a136766197803bf1889
Roberto Orellana <rorepoid@gmail.com>
2020-08-04 20:01:15 -0500

    feat: Use layout view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.jpeg') }}" type="image/x-icon"/>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <livewire:styles />
+</head>
+
+</head>
+<body>
+
+    <main class="w-full h-full">
+        <h1>rorepoid</h1>
+        @yield('content')
+    </main>
+
+    <livewire:scripts />
+
+</body>
+</html>
