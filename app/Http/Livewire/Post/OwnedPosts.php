975277f52f66586d38482d92967e79a492c7463e
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 06:26:15 -0500

    feat: User can see owned posts

--- app/Http/Livewire/Post/OwnedPosts.php
+++ app/Http/Livewire/Post/OwnedPosts.php
@@ -6,8 +6,19 @@ use Livewire\Component;
 
 class OwnedPosts extends Component
 {
+    public $posts;
+    public $order_by = 'desc';
+
     public function render()
     {
         return view('livewire.post.owned-posts');
+    }
+
+    public function mount()
+    {
+        $this->posts = auth()->user()
+                            ->posts()
+                            ->orderBy('updated_at', $this->order_by)
+                            ->get();
     }
 }
