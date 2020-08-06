e61316409862fd30e98b8553dd4265458e8f78ff
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 07:43:49 -0500

    feat: Add dropdown to change sort

--- app/Http/Livewire/Post/ListAllPosts.php
+++ app/Http/Livewire/Post/ListAllPosts.php
@@ -12,10 +12,16 @@ class ListAllPosts extends Component
 
     public function render()
     {
+        $this->getPosts();
         return view('livewire.post.list-all-posts');
     }
 
     public function mount()
+    {
+        $this->getPosts();
+    }
+
+    public function getPosts()
     {
         $this->posts = Post::orderBy('updated_at', $this->order_by)->get();
     }
--- resources/views/livewire/post/list-all-posts.blade.php
+++ resources/views/livewire/post/list-all-posts.blade.php
@@ -2,6 +2,12 @@
     <div class="container w-full h-full bg-gray-400">
         <div class="flex flex-col justify-center m-8">
             <h1 class="text-4xl font-bold font-mono">All Posts</h1>
+            <div>
+                <select wire:model="order_by" class="p-4 rounded-lg bg-gray-600 text-justify text-white font-bold" name="order_by">
+                    <option value="desc">Newer</option>
+                    <option value="asc">Older</option>
+                </select>
+            </div>
         </div>
             <div class="flex justify-center flex-wrap">
                 @foreach($posts as $post)
