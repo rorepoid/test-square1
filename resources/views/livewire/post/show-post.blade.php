6a42331cbf96d33754eaed9b3c1ebb88746559fa
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 09:39:34 -0500

    feat: Set friendly format to updated_at

--- resources/views/livewire/post/show-post.blade.php
+++ resources/views/livewire/post/show-post.blade.php
@@ -1,7 +1,8 @@
 <div class="w-full h-full flex justify-center overflow-y-auto">
     <div class="container w-full h-full px-3 lg:px-8">
-        <div class="flex justify-between m-8">
+        <div class="m-8">
             <h1 class="text-3xl xl:text-5xl font-bold">{{ $post->title }}</h1>
+            <span class="text-gray-600">Author: {{ $post->user->name }} - {{ \Carbon\Carbon::createFromTimeStamp(strtotime($post->updated_at))->diffForHumans() }}</span>
         </div>
             <div class="flex justify-center flex-wrap px-3">
                 <p class="text-xl font-mono text-left">{{ $post->body }}</p>
