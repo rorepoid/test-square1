f8200724e91aab65005c75c82782bf788ed2a6d9
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 04:55:19 -0500

    fix: Set post body attribute as long text

--- database/migrations/2020_08_06_064947_create_posts_table.php
+++ database/migrations/2020_08_06_064947_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('title');
-            $table->string('body');
+            $table->longText('body');
             $table->timestamps();
         });
     }
