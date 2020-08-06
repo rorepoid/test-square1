3e80dc2106dcc06d81b85240e8ad7b799d4cf01a
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 02:57:33 -0500

    test: Create test to validate if auth users can create posts

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,10 +2,31 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /** @test */
+    public function auth_users_can_create_posts()
+    {
+        // Arrange
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        // Act
+        $content = [
+            'title' => "The world's leading software development platform",
+            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolorum nulla, distinctio accusantium nostrum sapiente maiores fuga deserunt officia, maxime voluptatum earum porro non quidem facere perferendis, doloribus sequi? Laborum.",
+        ];
+
+        $post = $user->posts()->create($content);
+
+        // Assert
+        $this->assertEquals($user->id, $post->user_id);
+    }
 }
