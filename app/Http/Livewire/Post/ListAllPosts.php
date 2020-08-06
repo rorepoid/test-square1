e62c6e3f6cd75dec700770ce15b73af358de28a8
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 06:07:05 -0500

    feat: Order posts by

--- app/Http/Livewire/Post/ListAllPosts.php
+++ app/Http/Livewire/Post/ListAllPosts.php
@@ -8,6 +8,7 @@ use Livewire\Component;
 class ListAllPosts extends Component
 {
     public $posts;
+    public $order_by = 'desc';
 
     public function render()
     {
@@ -16,6 +17,6 @@ class ListAllPosts extends Component
 
     public function mount()
     {
-        $this->posts = Post::all();
+        $this->posts = Post::orderBy('updated_at', $this->order_by)->get();
     }
 }
