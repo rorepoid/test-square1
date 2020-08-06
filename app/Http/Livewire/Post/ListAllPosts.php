c444e888b12a2404abed41f25e7ac8d81edfabd2
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 05:56:23 -0500

    feat: List all posts

--- app/Http/Livewire/Post/ListAllPosts.php
+++ app/Http/Livewire/Post/ListAllPosts.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\Post;
 
+use App\Post;
 use Livewire\Component;
 
 class ListAllPosts extends Component
 {
+    public $posts;
+
     public function render()
     {
         return view('livewire.post.list-all-posts');
+    }
+
+    public function mount()
+    {
+        $this->posts = Post::all();
     }
 }
--- resources/views/livewire/post/list-all-posts.blade.php
+++ resources/views/livewire/post/list-all-posts.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="w-full h-full flex justify-center bg-gray-300 overflow-y-auto">
+    <div class="container w-full h-full bg-gray-400">
+        <div class="flex flex-col justify-center m-8">
+            <h1 class="text-4xl font-bold font-mono">All Posts</h1>
+        </div>
+            <div class="flex justify-center flex-wrap">
+                @foreach($posts as $post)
+                    <livewire:post.post-card  :post="$post" :key="$post->id"/>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </div>
