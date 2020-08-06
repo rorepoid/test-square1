aeef62ed8b9e5170cc65c43259ff0d0b83dc54cf
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 06:06:09 -0500

    feat: Show time ago

--- resources/views/livewire/post/post-card.blade.php
+++ resources/views/livewire/post/post-card.blade.php
@@ -1,4 +1,4 @@
 <div class="bg-white w-5/6 lg:w-7/12 mb-6 p-6 rounded-lg border-2 border-blue-700">
     <h1 class="text-2xl font-semibold mb-4">{{ $post->title }}</h1>
-    <span class="mt-16">{{ $post->updated_at }}</span>
+    <span class="mt-16">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($post->updated_at))->diffForHumans() }}</span>
 </div>
