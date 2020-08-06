4273807f06b4093dba13ea83b9b612d3b1046743
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 03:24:51 -0500

    feat: Create component to create posts

---
+++ app/Http/Livewire/Post/CreatePost.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Post;
+
+use Livewire\Component;
+
+class CreatePost extends Component
+{
+    public function render()
+    {
+        return view('livewire.post.create-post');
+    }
+}
