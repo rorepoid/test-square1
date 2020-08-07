a6ae98b149b593909642eed51257a32d1215636c
Roberto Orellana <rorepoid@gmail.com>
2020-08-06 19:51:24 -0500

    test: Assert getting data from api

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -4,8 +4,22 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ApiTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public $endpoint = 'https://sq1-api-test.herokuapp.com/posts';
+
+    /** @test */
+    public function get_posts_from_api_endpoint()
+    {
+        // Act
+        $response = Http::get($this->endpoint);
+
+        // Assert
+        $this->assertTrue($response->ok());
+    }
 }
