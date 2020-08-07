2697ffd2e422ad4d1d432f35360c1af22f1fd87f
Roberto Orellana <rorepoid@gmail.com>
2020-08-07 08:38:11 -0500

    feat: Add pagination

--- app/Http/Livewire/Post/ListAllPosts.php
+++ app/Http/Livewire/Post/ListAllPosts.php
@@ -9,25 +9,20 @@ use Livewire\Component;
 
 class ListAllPosts extends Component
 {
-    public $posts;
     public $order_by = 'desc';
 
     public function render()
     {
-        $this->getPosts();
-        return view('livewire.post.list-all-posts');
-    }
-
-    public function mount()
-    {
-        $this->getPosts();
+        return view('livewire.post.list-all-posts', ['posts' => $this->getPosts()]);
     }
 
     public function getPosts()
     {
-        $this->posts = Cache::remember("post.all.{$this->order_by}", 60, function () {
+        $page = request()->input('page', 1);
+
+        return Cache::remember("post.all.page_{$page}.{$this->order_by}", 60, function () {
             Post::importDataFromApi(new Square1);
-            return Post::orderBy('updated_at', $this->order_by)->get();
+            return Post::orderBy('updated_at', $this->order_by)->paginate(10);
         });
     }
 }
